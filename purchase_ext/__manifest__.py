68e276da7421b99b419b7be94b8110503ce51103
Htet Aung Shane <mmc.htetaungshane@gmail.com>
2025-11-11 03:00:39 +0000

    Purchase Extension

---
+++ purchase_ext/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Purchase Extension",
+    "summary": "Purchase Extension for Global Discount",
+    "description": """Purchase Extension for Global Discount""",
+    "author": "Htet Aung Shane",
+    "sequence": "1",
+    "website": "https://samplewebsite.com",
+    "category": "Customizations",
+    "version": "0.1",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+        "purchase",
+    ],
+    # always loaded
+    "data": [
+        # views
+        "views/purchase_order.xml",
+    ],
+    "application": True,
+    "license": "LGPL-3",
+}
---
+++ purchase_ext/models/purchase_order_line.py
@@ -0,0 +1,12 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class DMSPurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    gross_unit_price = fields.Float(string='Gross Unit Price', digits='Product Price')
+    total_before_global_discount = fields.Float(string='Total Before Global Discount', digits='Product Price')
+    global_discount_status = fields.Boolean(string='Global Discount Status', default=False)
+    global_discount_amount = fields.Float(string='Global Discount Amount', digits='Product Price')
+    net_amount = fields.Float(string='Net Amount', digits='Product Price')
